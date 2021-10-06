9c7e1c7497bc17bb8248fe7799279a55453a5777
HongMinh <minhbakaluffy114@gmail.com>
2021-10-06 13:31:20 +0700

    first commit

---
+++ Lập trình web/ViewJS/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.form_convert').click(function(event){
+        $('.form_Register').toggle();('mystyle');
+        $('.form_SignUp').toggle('block');
+    })
+})
